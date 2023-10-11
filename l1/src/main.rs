75b33c917e377764033d2b4567e1b7499331bb49
Spiryd <neumann.maks@gmail.com>
2023-10-11 21:48:37 +0200

    file ingest

--- l1/src/main.rs
+++ l1/src/main.rs
@@ -1,3 +1,36 @@
+use std::fs::{self, File};
+use std::io::{self, BufRead};
+use std::path::Path;
+
+type Point = (u16, u16);
+
 fn main() {
-    println!("Hello, world!");
+    let paths = fs::read_dir("data/").unwrap();
+    for path in paths {
+        let points = file_to_points(path.unwrap().path());
+        println!("{:?}", points)
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path> {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn file_to_points<P>(filename: P) -> Vec<Point> 
+where P: AsRef<Path> {
+    let mut points: Vec<Point> = Vec::new();
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines.skip(8) {
+            if let Ok(line) = line {
+                if line == "EOF"{
+                    break;
+                }
+                let tmp = line.split_whitespace().collect::<Vec<&str>>();
+                points.push((tmp[1].parse().unwrap(), tmp[2].parse().unwrap()));
+            }
+        }
+    }
+    points
 }
