cc8ce835a7a9321b2eba99ddf66200e42afb9d75
Spiryd <neumann.maks@gmail.com>
2023-10-14 21:45:00 +0200

    implemented prim's algo

--- l1/src/main.rs
+++ l1/src/main.rs
@@ -2,15 +2,80 @@ use std::fs::{self, File};
 use std::io::{self, BufRead};
 use std::path::Path;
 
-type Point = (u16, u16);
+type Point = (f32, f32);
 
 fn main() {
     let paths = fs::read_dir("data/").unwrap();
     for path in paths {
         let points = file_to_points(path.unwrap().path());
-        println!("{:?}", points)
+        let point_count = points.len();
+        //println!("{:?}", points);
+        let mut adj_matrix = vec![vec![u32::MAX; point_count]; point_count];
+        for i in 0..point_count {
+            for j in i..point_count  {
+                if j != i {
+                    let p1 = points[i];
+                    let p2 = points[j];
+                    let dist = (((p1.0 - p2.0).powi(2) + (p1.1 - p2.1).powi(2)).sqrt()).round() as u32;
+                    adj_matrix[i][j] = dist;
+                    adj_matrix[j][i] = dist;
+                }
+            }
+        }
+        //println!("{:?}", adj_matrix);
+        let parent = prim(&adj_matrix, point_count);
+        //println!("{:?}", &mst);
+        let weight = weight(&parent, &adj_matrix);
+        println!("{:?}", &weight);
+        let mst = parent_to_adj_list(&parent, &adj_matrix);
+
     }
 }
+
+fn weight(tree: &Vec<usize>, adj_matrix: &Vec<Vec<u32>>) -> u32 {
+    let mut s = 0;
+    for i in 1..(tree.len()) {
+        s += adj_matrix[i][tree[i]]
+    }
+    s
+}
+
+fn parent_to_adj_list(parent: &Vec<usize>, adj_matrix: &Vec<Vec<u32>>) {
+    todo!()
+}
+
+fn prim(adj_matrix: &Vec<Vec<u32>>, point_count: usize) -> Vec<usize> {
+    let mut parent: Vec<usize> = vec![usize::MAX; point_count];
+    let mut key: Vec<u32> = vec![u32::MAX; point_count];
+    let mut mst_set: Vec<bool> = vec![false; point_count];
+
+    key[0] = 0;
+
+    for _ in 0..(point_count - 1) {
+        let u = min_key(&key, &mst_set, point_count);
+        mst_set[u] = true;
+        for v in 0..point_count {
+            if adj_matrix[u][v] != 0 && mst_set[v] == false && adj_matrix[u][v] < key[v] {
+                parent[v] = u;
+                key[v] = adj_matrix[u][v];
+            }
+        }
+    }
+
+    parent
+}
+
+fn min_key(key: &Vec<u32>, mst_set: &Vec<bool>, point_count: usize) -> usize {
+    let mut min = u32::MAX;
+    let mut min_index = 0;
+    for v in 0..point_count {
+        if mst_set[v] == false && key[v] < min {
+            min = key[v];
+            min_index = v;
+        }
+    }
+    min_index
+} 
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path> {
